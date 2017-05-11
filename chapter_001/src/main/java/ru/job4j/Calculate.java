f19b6dd5f7b1f3123f18e735b13b57f1da0103ec
KEHT1 <pospelo1988@mail.ru>
2017-05-11 11:46:12 +0300

    add java-info

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j;
-
+/**
+*Class Calculate решение задачи части 001 урок1.
+*@author spospelov
+*@since 10.05.2017
+*/
 public class Calculate {
+	/**
+    *Method main метод для запуска классов интерпритатором.
+    *@ param args - входящий аргумент
+	*/
  public static void main (String []args){
    System.out.print("Hello world.");
   }         
---
+++ package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Sergei Pospelov (mailto:pospelo1988@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
+ 
